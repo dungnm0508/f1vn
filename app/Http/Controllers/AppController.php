dea98bf190c6a894161d798044986cefda39747f
dungnm <dungnm@zotaobox.com>
2018-08-23 10:22:15 +0700

    ad

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -101,7 +101,6 @@ class AppController extends Controller
             ->join('race', 'race.id', '=', 'calendar.id_race')
             ->select('*')
             ->get();
-        fadfaffaf
         var_dump($races);die;
     }
 }
